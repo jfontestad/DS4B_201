5d905dd3f5e42af48db57d63771ebc67d89a9a4c
Steven Sanderson <spsanderson@gmail.com>
2020-05-11 15:50:34 -0400

    develop kpi section and add calculate_attrition_cost function

--- file1.R
+++ file1.R
@@ -68,4 +68,66 @@ dept_job_role_tbl %>%
 
 # Collect Information on Employee Attrition
 
-# Develop KPI's
+# Develop KPI's: Industry KPI's 8.8%
+dept_job_role_tbl %>%
+    group_by(Department, JobRole, Attrition) %>%
+    summarize(n = n()) %>%
+    ungroup() %>%
+    # Get Dept Attrition
+    group_by(Department, JobRole) %>%
+    mutate(pct = n / sum(n)) %>%
+    ungroup() %>%
+    filter(Attrition == "Yes") %>%
+    arrange(desc(pct)) %>%
+    mutate(
+        above_industry_avg = case_when(
+            pct > 0.088 ~ "Yes",
+            TRUE ~ "No"
+        )
+    )
+
+# 1D Uncover Prolems and Opportunities
+calculate_attrition_cost <- function(
+    
+    # Employee
+    n                          = 1
+    , salary                   = 80000
+    
+    # Direct Cost
+    , separation_cost          = 500
+    , vacancy_cost             = 10000
+    , acquisition_cost         = 4900
+    , placement_cost           = 3500
+    
+    # Productivity costs
+    , net_revenue_per_employee = 250000
+    , workdays_per_year        = 240
+    , workdays_position_open   = 40
+    , workdays_onboarding      = 60
+    , onboarding_efficiency    = 0.50
+){
+    
+    # Direct Costs
+    direct_cost <- sum(
+        separation_cost
+        , vacancy_cost
+        , acquisition_cost
+        , placement_cost
+        )
+    
+    # Lost Productivyt Costs
+    productivity_cost <- net_revenue_per_employee / workdays_per_year * 
+        (workdays_position_open + workdays_onboarding * onboarding_efficiency)
+    
+    # Savings of Salary & Benefits (Cost Reduction)
+    salary_benefit_reduction <- salary / workdays_per_year * workdays_position_open
+    
+    # Estimated Turnover for Employee
+    cost_per_employee <- direct_cost + productivity_cost - salary_benefit_reduction
+    
+    # Total Cost of Employee Turnover
+    total_cost <- n * cost_per_employee
+    
+    return(total_cost)
+}
+calculate_attrition_cost(n = 200)
