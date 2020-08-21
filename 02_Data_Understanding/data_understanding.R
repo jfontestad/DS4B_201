c6b21355ca4d4e1b54fc0cff4ea29fc36b796e98
Steven Sanderson <spsanderson@gmail.com>
2020-08-21 15:23:44 -0400

    skim, char vs num data review

--- 02_Data_Understanding/data_understanding.R
+++ 02_Data_Understanding/data_understanding.R
@@ -27,3 +27,33 @@ definitions_raw_tbl
 
 train_raw_tbl
 glimpse(train_raw_tbl)
+
+
+# Step 1: Data summarization ----------------------------------------------
+
+skim(train_raw_tbl)
+
+# Char data
+train_raw_tbl %>%
+    select_if(is.character) %>%
+    glimpse()
+
+train_raw_tbl %>%
+    select_if(is.character) %>%
+    map(unique)
+
+train_raw_tbl %>%
+    select_if(is.character) %>%
+    map(~ table(.) %>% prop.table())
+
+# Num data
+train_raw_tbl %>%
+    select_if(is.numeric) %>%
+    map(~ unique(.) %>% length())
+
+train_raw_tbl %>%
+    select_if(is.numeric) %>%
+    map_df(~ unique(.) %>% length()) %>%
+    gather() %>%
+    arrange(value) %>%
+    filter(value <= 10)
