a08c4493d8e513bb2b569f254dedf1c4f1ed5fb2
Steven Sanderson <spsanderson@gmail.com>
2020-09-24 15:30:38 -0400

    plot_h2o_visualization function, viz h2o model leaderboard

--- 04_Modeling/modeling_h20_automated_machine_learning.R
+++ 04_Modeling/modeling_h20_automated_machine_learning.R
@@ -111,3 +111,150 @@ predictions <- h2o.predict(
 )
 predictions_tbl <- predictions %>%
     as_tibble()
+
+stacked_ensemble_h20@allparameters
+
+
+# Viz the leader board ----------------------------------------------------
+
+data_transformed_tbl <- automl_models_h20@leaderboard %>%
+    as_tibble() %>%
+    mutate(model_type = str_split(model_id, "_", simplify = TRUE)[,1]) %>%
+    slice(1:10) %>%
+    rownames_to_column() %>%
+    mutate(
+        model_id = as_factor(model_id) %>% fct_reorder(auc)
+        , model_type = as.factor(model_type)
+    ) %>%
+    select(rowname, model_id, auc, logloss, model_type) %>%
+    pivot_longer(
+        cols = c(auc, logloss)
+        , names_to = "key"
+        , values_to = "value"
+        , names_ptypes = list(key = factor())
+    )
+
+data_transformed_tbl %>%
+    ggplot(
+        aes(
+            x = value
+            , y = model_id
+            , color = model_type
+        )
+    ) +
+    geom_point(size = 3) +
+    geom_label(
+        aes(
+            label = round(value, 2)
+            , hjust = "inward"
+        )
+    ) +
+    facet_wrap(~ key, scales = "free_x") +
+    theme_tq() +
+    scale_color_tq() +
+    labs(
+        title = "H2O Model Leader Board Metrics"
+        , subtitle = "Order By AUC"
+        , x = ""
+        , y = ""
+    )
+
+plot_h2o_visualization <- function(
+    h2o_leaderboard
+    , order_by = c("auc","logloss","aucpr","mean_per_class_error","rmse","mse")
+    , metrics_list = c("auc","logloss","aucpr","mean_per_class_error","rmse","mse")
+    , n_max = 20
+    , size = 4
+    , include_lbl = TRUE
+) {
+    
+    # Setup Inputs
+    order_by     <- tolower(order_by[[1]])
+    metrics_list <- tolower(metrics_list)
+    
+    leaderboard_tbl <- h2o_leaderboard %>%
+        as_tibble() %>%
+        mutate(model_type = str_split(model_id, "_", simplify = TRUE)[, 1]) %>%
+        rownames_to_column(var = "rowname") %>%
+        mutate(model_id = paste0(
+            rowname
+            , ". "
+            , as.character(model_id)
+            ) 
+            %>% as.factor()
+        ) %>%
+        select(rowname, model_id, model_type, metrics_list, order_by)
+    
+    # Transformation
+    if(order_by == "auc") {
+        
+        data_transformed_tbl <- leaderboard_tbl %>%
+            slice(1:n_max) %>%
+            mutate(
+                model_id = as_factor(model_id) %>% 
+                    fct_reorder(auc)
+                , model_type = as.factor(model_type)
+            ) %>%
+            pivot_longer(
+                cols = c(-model_id, -model_type, -rowname)
+                , names_to = "key"
+                , values_to = "value"
+                , names_ptypes = list(key = factor())
+            )
+        
+    } else if (order_by == "logloss") {
+        data_transformed_tbl <- leaderboard_tbl %>%
+            slice(1:n_max) %>%
+            mutate(
+                model_id = as_factor(model_id) %>% 
+                    fct_reorder(logloss) %>% 
+                    fct_rev()
+                , model_type = as.factor(model_type)
+            ) %>%
+            pivot_longer(
+                cols = c(-model_id, -model_type, -rowname)
+                , names_to = "key"
+                , values_to = "value"
+                , names_ptypes = list(key = factor())
+            )
+    } else {
+        stop(paste0("order_by = '", order_by, " is not currently supported. Use auc or logloss"))
+    }
+    
+    # Viz
+    g <- data_transformed_tbl %>%
+        ggplot(
+            aes(
+                x = value
+                , y = model_id
+                , color = model_type
+            )
+        ) +
+        geom_point(size = size) +
+        facet_wrap(~ key, scales = "free_x") +
+        theme_tq() +
+        scale_color_tq() +
+        labs(
+            title = "H2O Leaderboard Metrics"
+            , subtitle = paste0("Ordered by: ", toupper(order_by))
+            , y = "Model Position and ID"
+            , x = ""
+        )
+    
+    if(include_lbl) g <- g +
+            geom_label(
+                aes(
+                    label = round(value, 2)
+                    , hjust = "inward"
+                )
+            )
+    
+    return(g)
+}
+
+plot_h2o_visualization(
+    automl_models_h20@leaderboard
+    , order_by = c("auc")
+    , metrics_list = c("auc","aucpr")
+    , size = 3
+)
